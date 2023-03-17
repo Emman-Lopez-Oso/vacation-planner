eff6247de9f69caf44ff4092e075dabaf3e7d869
Emmanuel Lopez <emman_lopez@berkeley.edu>
2023-03-16 20:48:38 -0700

    Adding mechanism to change between months and individually select them

--- scripts.js
+++ scripts.js
@@ -3,23 +3,43 @@ const calendar = document.getElementById("calendar");
 const today = new Date();
 const startDate = new Date(today.getFullYear(), today.getMonth(), 1);
 
+const prevMonthButton = document.getElementById("prev-month");
+const nextMonthButton = document.getElementById("next-month");
+let currentMonthIndex = 0;
+
 function displaySelectedMonth() {
   const monthSelect = document.getElementById("month-select");
   const selectedMonths = Array.from(monthSelect.selectedOptions).map(option => parseInt(option.value));
   const monthNames = selectedMonths.map(month => new Date(2000, month, 1).toLocaleString("default", { month: "long" }));
-  document.getElementById("calendar").dataset.month = monthNames.join(", ");
+  document.getElementById("calendar").dataset.month = monthNames[currentMonthIndex];
+}
+
+function updateMonthNavigation() {
+  if (currentMonthIndex === 0) {
+    prevMonthButton.disabled = true;
+  } else {
+    prevMonthButton.disabled = false;
+  }
+
+  const monthSelect = document.getElementById("month-select");
+  const selectedMonths = Array.from(monthSelect.selectedOptions).map(option => parseInt(option.value));
+
+  if (currentMonthIndex === selectedMonths.length - 1) {
+    nextMonthButton.disabled = true;
+  } else {
+    nextMonthButton.disabled = false;
+  }
 }
 
 function updateCalendar() {
-  // Get selected months
   const monthSelect = document.getElementById("month-select");
   const selectedMonths = Array.from(monthSelect.selectedOptions).map(option => parseInt(option.value));
 
   // Clear the existing calendar
   calendar.innerHTML = "";
 
-  // Generate the new calendar for each selected month
-  selectedMonths.forEach(selectedMonth => {
+  if (selectedMonths.length > 0) {
+    const selectedMonth = selectedMonths[currentMonthIndex];
     const start = new Date(today.getFullYear(), selectedMonth, 1);
     const end = new Date(today.getFullYear(), selectedMonth + 1, 0);
 
@@ -30,7 +50,10 @@ function updateCalendar() {
       cell.textContent = day.getDate();
       calendar.appendChild(cell);
     }
-  });
+
+    displaySelectedMonth();
+    updateMonthNavigation();
+  }
 }
 
 // Call the functions to display the initial month and calendar
@@ -45,7 +68,17 @@ calendar.addEventListener("click", (e) => {
 });
 
 document.getElementById("month-select").addEventListener("change", () => {
-  displaySelectedMonth();
+  currentMonthIndex = 0;
+  updateCalendar();
+});
+
+prevMonthButton.addEventListener("click", () => {
+  currentMonthIndex--;
+  updateCalendar();
+});
+
+nextMonthButton.addEventListener("click", () => {
+  currentMonthIndex++;
   updateCalendar();
 });
 
